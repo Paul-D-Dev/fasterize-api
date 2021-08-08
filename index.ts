7b1dc6f3538b594280175250c3e7f52dda7c536b
Paul DAO <dao.van.paul@gmail.com>
2021-08-08 20:34:50 +0200

    FEAT get method url define status & headers

--- index.ts
+++ index.ts
@@ -1,10 +1,23 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
+import { getHeadersUrl } from './functions/getHeadersUrl';
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const PORT = 3004;
 
-app.get('/', (req: any, res: any) => {
-    res.status(200).send('Fasterize baby !');
+app.get('/:url', async (req: any, res: any) => {
+    const url: string = req.params.url;
+
+    const responseRequestUrl: any =  await getHeadersUrl(url);
+
+    const status: number = responseRequestUrl.status;
+    const headers = responseRequestUrl.headers;
+
+
 })
 
 app.listen(PORT, () => {
