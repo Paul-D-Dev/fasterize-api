9a3b6a896939121c01bf71df96c5c43aa8e608a9
Paul DAO <dao.van.paul@gmail.com>
2021-08-10 23:54:09 +0200

    FEAT index get :url : catch error and fix some error

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { findCity } from './functions/findCity';
 import { getHeadersUrl } from './functions/getHeadersUrl';
 import { getLocationJson } from './functions/getLocationJson';
@@ -14,34 +14,51 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const PORT = 3004;
 
-app.get('/:url', async (req: any, res: any) => {
+app.get('/:url', async (req: Request, res: Response) => {
     const url: string = req.params.url;
     const locationJson: EdgeLocations = await getLocationJson();
-    const responseRequestUrl: any =  await getHeadersUrl(url);
+    let responseRequestUrl: any;
+    let status: number;
+    let headers: Headers;
+    let error: Error = {} as Error;
+    const plug: ResponsePlug = {} as ResponsePlug;
 
-    const status: number = responseRequestUrl.status;
-    const headers = responseRequestUrl.headers;
-
-    let flag = headers.get('x-fstrz');
-    flag = transformFlag(flag);
-    const cfPop = headers.get('x-amz-cf-pop');
-    const cache = headers.get('x-cache');
-
-    const plug: ResponsePlug = {
-        plugged: flag?.length > 0 ? true: false,
-        statusCode: status,
-        fstrzFlags: flag || [],
-        cloudfrontStatus: cache?.split(' ')[0].toUpperCase() || '',
-        cloudfrontPOP: findCity(locationJson, cfPop)
-    }
-    console.log(plug);
-    
     try {
-        await res.status(200).send(plug);
+        responseRequestUrl =  await getHeadersUrl(url);
+        status = responseRequestUrl.status || null;
+        headers = responseRequestUrl.headers;
+        let flag: string | null = headers.get('x-fstrz') || null;
+        console.log('flag', flag);
+        const flags: string[] = transformFlag(flag);
         
-    } catch (error) {
-        await res.status(400).send('bad request');
+        if (flags.includes('optimisée') || flags.length > 0) {
+            plug.plugged = true;
+        } else {
+            plug.plugged = false;
+        }
+        plug.statusCode = status;
+        if (plug.plugged) {
+            const cfPop = headers?.get('x-amz-cf-pop');
+            const cache = headers?.get('x-cache');
+            plug.fstrzFlags = flags
+            plug.cloudfrontStatus = cache?.split(' ')[0].toUpperCase() || ''
+            plug.cloudfrontPOP = findCity(locationJson, cfPop)
+        }
+
+        console.log(plug);
+
+        res.status(200).send(plug);
+
+    } catch (err) {
+        error = err;
+        console.error('headers', error);
+        if (error.message === 'HEADER UNDEFINED') {
+            return res.status(404).send('HEADER UNDEFINED');
+        }
     }
+
+    
+
 })
 
 app.listen(PORT, () => {
