8af19693c884e3b8562664001e62c5ffc834b1a1
Paul DAO <dao.van.paul@gmail.com>
2021-08-10 23:52:07 +0200

    FEAT transformFlag increases object values to translate flags

--- functions/transformFlag.ts
+++ functions/transformFlag.ts
@@ -4,8 +4,8 @@
  * @param flag string
  * @returns string's array 
  */
-export const transformFlag = (flag: string): string[] => {
-    if (flag === undefined || flag === null) {
+export const transformFlag = (flag: string | null): string[] => {
+    if (flag === undefined || flag === null || flag === '') {
         return [];
     } else {
         const flags = flag.split(',');
@@ -13,27 +13,36 @@ export const transformFlag = (flag: string): string[] => {
     }
 }
 
-const values: any = {
-    h: "contenu qui n'est pas du HTML",
-    o: "optimisée",
-    '!o': "non optimisée",
-    c: "cachée",
-    '!c': "non cachée",
-    'o,c': "optimisée, cachée",
-    'w,c': "réponse en cours d'optimisation mais servie cachée",
-    'dc,o': "optimisée, cache dynamique",
-    sc: "SmartCache",
-    clc: "Cookie Less Cache",
-    pl: "des liens de preload ont été insérés",
-    ed: "moteur désactivé",
-    v: "objet virtuel",
-    vf: "objet virtuel reconstitué",
-    t: "timeout",
-    tb: "too big",
-    e: "error",
-    b: "blocked",
-    uc: "User Canceled",
-    cbo: "Circuit Breaker Open",
-    ccb: "Cache CallBack",
-    of: "Overflow",
+const values: {[key: string]: string} = {
+    p       : "requête/réponse proxifiée",
+    'w,p'   : "optimisation en cours",
+    'f,p'   : "front/forward, proxy",
+    'Z,p'   : "page non optimisable",
+    'ecc'   : "exclusion via la config client",
+    'eec'   : "exclusion via la config du moteur",
+    'stc'   : "status code de l'origine différent de 200",
+    'ab'    : "exclusion par un test A/B",
+    'zc'    : "contenu vide",
+    h       : "contenu qui n'est pas du HTML",
+    o       : "optimisée",
+    '!o'    : "non optimisée",
+    c       : "cachée",
+    '!c'    : "non cachée",
+    'o,c'   : "optimisée, cachée",
+    'w,c'   : "réponse en cours d'optimisation mais servie cachée",
+    'dc,o'  : "optimisée, cache dynamique",
+    sc      : "SmartCache",
+    clc     : "Cookie Less Cache",
+    pl      : "des liens de preload ont été insérés",
+    ed      : "moteur désactivé",
+    v       : "objet virtuel",
+    vf      : "objet virtuel reconstitué",
+    t       : "timeout",
+    tb      : "too big",
+    e       : "error",
+    b       : "blocked",
+    uc      : "User Canceled",
+    cbo     : "Circuit Breaker Open",
+    ccb     : "Cache CallBack",
+    of      : "Overflow",
 }
