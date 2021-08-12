e2cafb64f075b4d2f0d55393f148b390e9cf4ba0
Paul DAO <dao.van.paul@gmail.com>
2021-08-12 22:32:29 +0200

    FEAT test http protocol

--- functions/validUrl.test.ts
+++ functions/validUrl.test.ts
@@ -2,8 +2,12 @@ import { validUrl }from './validUrl';
 
 test('Should return true', () => {
     expect(validUrl('https://www.fasterize.com')).toBeTruthy;
-})
+});
+
+test('Should return true 2', () => {
+    expect(validUrl('http://www.fasterize.com')).toBeTruthy;
+});
 
 test('Should return false', () => {
     expect(validUrl('www.fasterize.com')).toBeFalsy;
-})
+});
